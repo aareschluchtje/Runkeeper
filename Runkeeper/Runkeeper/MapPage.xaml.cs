ebc57d792ccc7e9a570f62198ed0a4a989fb5443
Aareschluchtje <remcosannen@gmail.com>
2015-12-15 14:56:07 +0100

    Added current location

--- Runkeeper/Runkeeper/MapPage.xaml.cs
+++ Runkeeper/Runkeeper/MapPage.xaml.cs
@@ -4,9 +4,12 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
+using Windows.System;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
@@ -14,6 +17,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Devices.Geolocation;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -27,7 +32,7 @@ namespace Runkeeper
         public MapPage()
         {
             this.InitializeComponent();
-            GetPosition();
+            currentLocation();
         }
 
         public async static Task<Geoposition> GetPosition()
@@ -36,14 +41,24 @@ namespace Runkeeper
 
             if (accesStatus != GeolocationAccessStatus.Allowed)
             {
-                throw new Exception();
+                var succes = await Launcher.LaunchUriAsync(new Uri("ms-settings:privacy-location"));
             }
-
-            var geolocator = new Geolocator { DesiredAccuracyInMeters = 0 };
+            var geolocator = new Geolocator { DesiredAccuracyInMeters = 0 , ReportInterval = 1000};
 
             var position = await geolocator.GetGeopositionAsync();
 
             return position;
+        }
+
+        private async void currentLocation()
+        {
+            Geoposition position = await GetPosition();
+            MapControl1.Center = position.Coordinate.Point;
+            MapControl1.ZoomLevel = 16;
+            MapIcon runner = new MapIcon();
+            runner.Location = position.Coordinate.Point;
+            runner.Title = "I am here";
+            MapControl1.MapElements.Add(runner);
         }
     }
 }
